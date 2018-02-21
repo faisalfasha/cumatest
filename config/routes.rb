9d03cacaeae1a509f8fa5b32fb37e966b8d6ca21
faisalfasha <faisal.f@41studio.com>
2018-02-21 10:35:34 +0700

    takut salah

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  # get 'stpre/index'
+  root 'stpre#index', as: 'stpre_index'
+
+  resources :keduas
+  resources :products
+  get 'say/hello'
+
+  get 'say/goodbye'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
